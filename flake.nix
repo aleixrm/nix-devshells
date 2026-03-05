ec16ab1a20dbf891ce13203e2689ea237a62a111
Aleix Ramírez <aleix.rmbaena@gmail.com>
2026-03-05 19:24:39 +0100

    Repo restructuration
    Signed-off-by: Aleix Ramírez <aleix.rmbaena@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,31 +1,14 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
+  description = "Templates for different languages devenvs";
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    templates = {
+
+      rust = {
+        path = ./templates/rust;
+        description = "Template for a rust devenv";
       };
+
     };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }: 
-  flake-utils.lib.eachDefaultSystem 
-    (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        nativeBuildInputs = [ rustToolchain ];
-        buildInputs = with pkgs; [];
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          inherit buildInputs nativeBuildInputs;
-        };
-      }
-    );
 }
